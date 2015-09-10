a3f3929129306ff7323974f27d8f9c79afc7bb23
Matt Hayes <mhayes@tumblr.com>
2015-09-09 22:52:34 -0700

    Add a rough table view with auto layout.

--- Welp/BusinessesViewController.swift
+++ Welp/BusinessesViewController.swift
@@ -8,28 +8,55 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+extension NSLayoutFormatOptions {
+    static var None: NSLayoutFormatOptions {
+        return NSLayoutFormatOptions(0)
+    }
+}
+
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var businesses: [Business]?
+    
+    var tableView: UITableView!
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        tableView = UITableView()
+        
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         self.title = "Welp"
+
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         view.backgroundColor = UIColor.randomColor()
+
+        view.addSubview(tableView)
+        tableView.backgroundColor = UIColor.randomColor()
+        let views = ["tableView": tableView]
+        
+        let VFLH = "H:|-8-[tableView]-8-|"
+        let VFLV = "V:|-8-[tableView]-8-|"
+        
+        let tableViewContraintH = NSLayoutConstraint.constraintsWithVisualFormat(VFLH, options: .None, metrics: nil, views: views)
+        let tableViewContraintV = NSLayoutConstraint.constraintsWithVisualFormat(VFLV, options: .None, metrics: nil, views: views)
         
         Yelp.instance.search("Restaurants", completion: {
             (businesses: [Business]!, error: NSError!) -> Void in
             
-            if let businesses = businesses {
-                businesses.map({business in println(business.name!)})
-            }
+//            if let businesses = businesses {
+//                businesses.map({business in println(business.name!)})
+//            }
+            
+            self.businesses = businesses
         });
     }
 
@@ -38,6 +65,14 @@ class BusinessesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BusinessViewCell", forIndexPath: indexPath) as! UITableViewCell
+        return cell
+    }
 
 }
 
